0a0d02e4f4172fa61ec2f168143186704f7af324
yen_lee <i745m@hotmail.com>
2014-08-04 23:25:46 +0800

    現在可以對動態載入的字做修改，但會嚴重影響到Firefox的效能

--- JiZhiWu.user.js
+++ JiZhiWu.user.js
@@ -7,10 +7,15 @@
 // @description   Replace some texts with 祭止兀
 	 
 // @include       *
+
+// @grant         GM_log
 	 
 // ==/UserScript==
 //alert('Ji Zhi Wu filter');
-textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+function init() {
+	
+}
+
 function replace(original, replaced) {
 	var searchRE = new RegExp(original,'g');
 	for (var i=0; i<textNodes.snapshotLength; ++i) {
@@ -19,5 +24,38 @@ function replace(original, replaced) {
 	}
 }
 
-replace("蔡正元", "祭止兀");
-replace("正元", "止兀");
+function mainStuff() {
+	textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	console.log('mainStuff triggered');
+	replace("蔡正元", "祭止兀");
+	replace("正元", "止兀");	
+}
+
+function newPostsLoaded (zEvent) {
+	console.log('newPostsLoaded triggered');
+    /*--- Set and reset a timer so that we run our code (LocalMain() ) only
+        AFTER the last post -- in a batch -- is added.  Adjust the time if needed, but
+        half a second is a good all-round value.
+    */
+	if (typeof PostsChangedByAJAX_Timer == "number") {
+		clearTimeout (PostsChangedByAJAX_Timer);
+		PostsChangedByAJAX_Timer = '';
+	}
+	PostsChangedByAJAX_Timer = setTimeout (function() {mainStuff (); }, 555);
+}
+
+if (window.top != window.self) {//don't run on frames or iframes
+	console.log("window.top != window.self");
+	return;
+} 
+	
+
+init();
+mainStuff();
+
+console.log("hello this is greasemonkey");
+var PostsChangedByAJAX_Timer = '';
+
+document.addEventListener ("DOMSubtreeModified", newPostsLoaded, false);
+
+
