21d00fe66dc646b68aaa34903d7bdaf2e0dcb92f
yen_lee <i745m@hotmail.com>
2014-08-06 21:37:43 +0800

    Improve performance

--- JiZhiWu.user.js
+++ JiZhiWu.user.js
@@ -7,8 +7,6 @@
 // @description   Replace some texts with 祭止兀
 	 
 // @include       *
-
-// @grant         GM_log
 	 
 // ==/UserScript==
 //alert('Ji Zhi Wu filter');
@@ -17,44 +15,47 @@ function replace(node, original, replaced) {
 	node.data = node.data.replace(original, replaced);
 }
 
-function mainStuff() {
+function replaceText() {
 	// //: Selects nodes in the document from the current node that match the selection no matter where they are
 	
 	textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-	//console.log('mainStuff triggered');
-	console.log('textNodes.snapshotLength: ' + textNodes.snapshotLength);
+	console.log('replaceText triggered');
+	//console.log('textNodes.snapshotLength: ' + textNodes.snapshotLength);
 	for (var i=0; i<textNodes.snapshotLength; ++i) {
 		var node = textNodes.snapshotItem(i);
 		replace(node, "蔡正元", "祭止兀");
 		replace(node, "正元", "止兀");
 	}
+	resetTimer();
+}
+
+function hasTimerTriggered() {
+	return (typeof PostsChangedByAJAX_Timer == "number");
+}
+
+function resetTimer() {
+	clearTimeout (PostsChangedByAJAX_Timer);
+	PostsChangedByAJAX_Timer = '';
 }
 
 function newPostsLoaded (zEvent) {
-	//console.log('newPostsLoaded triggered');
+	console.log('newPostsLoaded triggered');
     /*--- Set and reset a timer so that we run our code (LocalMain() ) only
         AFTER the last post -- in a batch -- is added.  Adjust the time if needed, but
         half a second is a good all-round value.
     */
-	if (typeof PostsChangedByAJAX_Timer == "number") {
-		clearTimeout (PostsChangedByAJAX_Timer);
-		PostsChangedByAJAX_Timer = '';
-	}
-	PostsChangedByAJAX_Timer = setTimeout (function() {mainStuff (); }, 555);
+    if ( !hasTimerTriggered() ) {
+    	PostsChangedByAJAX_Timer = setTimeout (function() {replaceText (); }, 555);
+    }
 }
 
-if (window.top != window.self) {//don't run on frames or iframes
-	console.log("window.top != window.self");
-	return;
-} 
-	
-
-
-mainStuff();
-
-//console.log("hello this is greasemonkey");
 var PostsChangedByAJAX_Timer = '';
 
-document.addEventListener ("DOMSubtreeModified", newPostsLoaded, false);
+/*if (window.top != window.self) { //don't run on frames or iframes
+	console.log("window.top != window.self");
+	return;
+} */	
 
+replaceText();
 
+document.addEventListener ("DOMNodeInserted", newPostsLoaded, false);
