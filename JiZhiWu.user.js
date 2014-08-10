1d9ef6e57a9918be40a6950ea2146d407213f660
yen_lee <i745m@hotmail.com>
2014-08-10 17:49:50 +0800

    Remove unnecessary search in the code.

--- JiZhiWu.user.js
+++ JiZhiWu.user.js
@@ -6,7 +6,7 @@
 	 
 // @description   Replace some texts with 祭止兀
 	 
-// @include       *
+// @include       https://www.facebook.com/*
 	 
 // ==/UserScript==
 //alert('Ji Zhi Wu filter');
@@ -17,10 +17,9 @@ function replace(node, original, replaced) {
 
 function replaceText() {
 	// //: Selects nodes in the document from the current node that match the selection no matter where they are
-	
-	textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	textNodes = document.evaluate("//a/text() | //span/text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 	console.log('replaceText triggered');
-	//console.log('textNodes.snapshotLength: ' + textNodes.snapshotLength);
+	console.log('textNodes.snapshotLength: ' + textNodes.snapshotLength);
 	for (var i=0; i<textNodes.snapshotLength; ++i) {
 		var node = textNodes.snapshotItem(i);
 		replace(node, "蔡正元", "祭止兀");
@@ -40,22 +39,21 @@ function resetTimer() {
 
 function newPostsLoaded (zEvent) {
 	console.log('newPostsLoaded triggered');
-    /*--- Set and reset a timer so that we run our code (LocalMain() ) only
-        AFTER the last post -- in a batch -- is added.  Adjust the time if needed, but
-        half a second is a good all-round value.
-    */
     if ( !hasTimerTriggered() ) {
     	PostsChangedByAJAX_Timer = setTimeout (function() {replaceText (); }, 555);
     }
 }
 
+function replaceTitle () {
+	document.title = document.title.replace('蔡正元', '祭止兀');
+}
+
 var PostsChangedByAJAX_Timer = '';
 
-/*if (window.top != window.self) { //don't run on frames or iframes
-	console.log("window.top != window.self");
-	return;
-} */	
+if (window.top != window.self) { //don't run on frames or iframes
+    return;
+}
 
+replaceTitle();
 replaceText();
-
 document.addEventListener ("DOMNodeInserted", newPostsLoaded, false);
--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 祭止兀顯示器：
 
+當你用Firefox瀏覽facebook時，看到的「蔡正元」通通會變成「祭止兀」
+
 這份文件需要配合Firefox外掛greasemonkey來使用
 https://addons.mozilla.org/zh-tw/firefox/addon/greasemonkey/
-
-裝好後，Firefox看到的蔡正元三個字都會被替換成祭止兀
